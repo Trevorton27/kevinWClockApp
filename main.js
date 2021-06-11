c9589ce4ab2122b96ad0fc1ed5803fde280bb735
Kevin <85580394+KevinWestNYC@users.noreply.github.com>
2021-06-11 12:15:29 -0400

    Add files via upload
    all js functionality complete less interval

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let d = new Date();
+
+//DATE
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let day = days[d.getDay()];
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -23,13 +25,29 @@ let year = d.getFullYear();
 let calendar = document.getElementById("calendar")
 calendar.textContent = `${day}, ${month}  ${date}${suffix(date)}  ${year}`;
 
+//TIME
+let hr = d.getHours();
+let hour = hr => {
+    if (hr > 12){
+        return hr - 12
+        } else if( hr === 0){
+            return 12
+        } else {
+            return hr
+        }
+       }
+let zero = () =>{
+    return hour(hr) < 10 ? '0' : '';
+}
+let amPm = hr => hr > 11 ? 'PM' : 'AM'
+//let hours = hour => {return hour < 10 ? '0' + hour : hour}
+let min = d.getMinutes();
+let minute = min => {return min < 10 ? '0' + min : min}
+let sec = d.getSeconds();
+let second = sec => {return sec < 10 ? '0' + sec : sec}
+    
 
-let hour = d.getHours();
-let minute = d.getMinutes();
-let second = d.getSeconds();
+
 
 let clock = document.getElementById("clock")
-clock.textContent = `${hour}:${minute}:${second}`;
-
-//day month date year  (eg. Monday, August 12th 2021)
-// HOURS:MINUTES:SECONDS (eg. 03:30:09)
+clock.textContent = `${zero()}${hour(hr)}:${minute(min)}:${second(sec)}  ${amPm(hr)}`;
